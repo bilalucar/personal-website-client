11cefce1d2bc7029a64ce86dd5ebdd7ee130a675
bilalucar <f44aaf4133>
2020-11-26 17:26:55 +0300

    Add svgs to header menu

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,6 +7,16 @@ import Portfolio from './Portfolio';
 import BlogDetail from './Blog-Detail';
 import PortfolioItem from '../components/Portfolio-Item';
 import Footer from '../components/Footer';
+import SvgHome from "../components/icons/Home";
+import SvgSuitcase from "../components/icons/Suitcase";
+import SvgWriting from "../components/icons/Writing";
+import SvgContact from "../components/icons/Contact";
+import SvgCv from "../components/icons/Cv";
+import SvgHomeFilled from "../components/icons/HomeFilled";
+import SvgSuitcaseFilled from "../components/icons/SuitcaseFilled";
+import SvgWritingFilled from "../components/icons/WritingFilled";
+import SvgCvFilled from "../components/icons/CvFilled";
+import SvgContactFilled from "../components/icons/ContactFilled";
 
 class App extends React.Component<any, any> {
 
@@ -16,11 +26,26 @@ class App extends React.Component<any, any> {
                 <Router>
                     <div id="header" className="header">
                         <div className="header__menu">
-                            <Link id="home" className="header__menu-item" to="/">Anasayfa</Link>
-                            <Link id="portfolio" className="header__menu-item" to="/portfolyo">Portfolyo</Link>
-                            <Link className="header__menu-item" to="/blog">Blog</Link>
-                            <Link className="header__menu-item" target="_blank" to="/files/bilal-ucar-cv.pdf">CV</Link>
-                            <a className="header__menu-item" href="/#contact">İletişim</a>
+                            <Link id="home" className="header__menu-item" to="/">
+                                <SvgHomeFilled/>
+                                <span>Anasayfa</span>
+                            </Link>
+                            <Link id="portfolio" className="header__menu-item" to="/portfolyo">
+                                <SvgSuitcaseFilled/>
+                                <span>Portfolyo</span>
+                            </Link>
+                            <Link className="header__menu-item" to="/blog">
+                                <SvgWritingFilled/>
+                                <span>Blog</span>
+                            </Link>
+                            <Link className="header__menu-item" target="_blank" to="/files/bilal-ucar-cv.pdf">
+                                <SvgCvFilled/>
+                                <span>CV</span>
+                            </Link>
+                            <a className="header__menu-item" href="/#contact">
+                                <SvgContactFilled/>
+                                <span>İletişim</span>
+                            </a>
                         </div>
                     </div>
 
