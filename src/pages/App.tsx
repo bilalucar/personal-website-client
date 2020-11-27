c8061142c275d4a8fba59bc64d44b83aedc1d0d8
bilalucar <f44aaf4133>
2020-11-28 01:25:03 +0300

    Add menu active class

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
+import classNames from 'classnames';
+
 import Home from './Home';
 import Blog from './Blog';
 import Portfolio from './Portfolio';
@@ -8,17 +10,69 @@ import BlogDetail from './Blog-Detail';
 import PortfolioItem from '../components/Portfolio-Item';
 import Footer from '../components/Footer';
 import SvgHome from "../components/icons/Home";
-import SvgSuitcase from "../components/icons/Suitcase";
-import SvgWriting from "../components/icons/Writing";
-import SvgContact from "../components/icons/Contact";
-import SvgCv from "../components/icons/Cv";
-import SvgHomeFilled from "../components/icons/HomeFilled";
 import SvgSuitcaseFilled from "../components/icons/SuitcaseFilled";
 import SvgWritingFilled from "../components/icons/WritingFilled";
 import SvgCvFilled from "../components/icons/CvFilled";
 import SvgContactFilled from "../components/icons/ContactFilled";
+import SvgHomeFilled from "../components/icons/HomeFilled";
+import SvgSuitcase from "../components/icons/Suitcase";
+import SvgWriting from "../components/icons/Writing";
+import SvgCv from "../components/icons/Cv";
+import SvgContact from "../components/icons/Contact";
+
+interface MenuItem {
+    text: string;
+    path: string;
+    icon: any;
+    iconHover: any;
+}
+
+const menuItems = [
+    {
+        text: 'Anasayfa',
+        path: '/',
+        icon: <SvgHome/>,
+        iconHover: <SvgHomeFilled/>
+    },
+    {
+        text: 'Portfolyo',
+        path: '/portfolyo',
+        icon: <SvgSuitcase/>,
+        iconHover: <SvgSuitcaseFilled/>
+    },
+    {
+        text: 'Blog',
+        path: '/blog',
+        icon: <SvgWriting/>,
+        iconHover: <SvgWritingFilled/>
+    },
+    {
+        text: 'CV',
+        path: '/files/bilal-ucar-cv.pdf',
+        icon: <SvgCv/>,
+        iconHover: <SvgCvFilled/>
+    },
+    {
+        text: 'İletişim',
+        path: '/#contact',
+        icon: <SvgContact/>,
+        iconHover: <SvgContactFilled/>
+    }
+];
 
 class App extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            active: '/'
+        };
+    }
+
+    _handleClick(menuItem: MenuItem) {
+        this.setState({ active: menuItem.path });
+    }
 
     render() {
         return (
@@ -26,26 +80,20 @@ class App extends React.Component<any, any> {
                 <Router>
                     <div id="header" className="header">
                         <div className="header__menu">
-                            <Link id="home" className="header__menu-item" to="/">
-                                <SvgHomeFilled/>
-                                <span>Anasayfa</span>
-                            </Link>
-                            <Link id="portfolio" className="header__menu-item" to="/portfolyo">
-                                <SvgSuitcaseFilled/>
-                                <span>Portfolyo</span>
-                            </Link>
-                            <Link className="header__menu-item" to="/blog">
-                                <SvgWritingFilled/>
-                                <span>Blog</span>
-                            </Link>
-                            <Link className="header__menu-item" target="_blank" to="/files/bilal-ucar-cv.pdf">
-                                <SvgCvFilled/>
-                                <span>CV</span>
-                            </Link>
-                            <a className="header__menu-item" href="/#contact">
-                                <SvgContactFilled/>
-                                <span>İletişim</span>
-                            </a>
+                            {menuItems.map(menuItem => {
+                                const classes = classNames({
+                                    'header__menu-item': true,
+                                    'active': this.state.active === menuItem.path
+                                });
+                                return <Link
+                                    key={menuItem.path}
+                                    className={classes}
+                                    onClick={this._handleClick.bind(this, menuItem)}
+                                    to={menuItem.path}>
+                                    {this.state.active === menuItem.path ? menuItem.iconHover : menuItem.icon} <span>{menuItem.text}</span>
+                                </Link>
+                                }
+                            )}
                         </div>
                     </div>
 
